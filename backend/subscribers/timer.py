bdc4804cc50d0d46e272168ca7adf2856f956768
guacamole-hunter <windowtodevelopment@gmail.com>
2022-09-23 14:48:34 -0700

    odels

--- backend/subscribers/timer.py
+++ backend/subscribers/timer.py
@@ -1,9 +1,11 @@
 import datetime
 from .sendEmails import sendEmail
+from .hackathonData import getContests
 
 def timer():
     time = datetime.datetime.now()
     sendtime = time.strftime("%A")
     print (time)
     if sendtime == "Monday":
+        getContests()
         sendEmail()
